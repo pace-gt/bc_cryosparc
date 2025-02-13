7852ee390db969228f5d859561cd3ad1f9cad0d4
Ron Rahaman <rrahaman6@gatech.edu>
2025-02-13 17:38:40 -0500

    RR: Disable password manager in Firefox profile

--- template/firefox-profile/prefs.js
+++ template/firefox-profile/prefs.js
@@ -1,3 +1,5 @@
 user_pref("browser.link.open_newwindow.restriction", 0);
 user_pref("browser.link.open_newwindow.override.external", 3);
 user_pref("browser.link.open_newwindow", 1);
+user_pref("signon.rememberSignons", false);
+user_pref("signon.autofillForms", false);
